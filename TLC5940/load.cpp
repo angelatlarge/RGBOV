72268ddca14858387831d80edea9de644e4eb687
Kirill Shklovsky <kirill.shklovsky@gmail.com>
2013-02-18 00:31:46 -0500

    a number of speed optimizations

--- TLC5940/load.cpp
+++ TLC5940/load.cpp
@@ -2,7 +2,11 @@
 #include "../shared/load.h"
 #include "../shared/settings.h"
 
-#define COLUMN_DATA_BYTES		VERTICAL_PIXELS*3
+#if GRAPHIC_HEIGHT>VERTICAL_PIXELS
+#	define COLUMN_DATA_BYTES	(GRAPHIC_HEIGHT*3)
+#else
+#	define COLUMN_DATA_BYTES	(VERTICAL_PIXELS*3)
+#endif
 
 #if INTENSITY_LEVELS>1			
 #error For TLC5940 INTENSITY_LEVELS must be set at zero
@@ -37,6 +41,9 @@
 
 #include "../shared/graphic.c"
 
+#define PRECOMPUTE_PALETTE		/* 	Optimization that does palette exponentiation at initialization.  
+									Can saves about 51.2us of the loading time
+									*/
 /*
 	We have a vertical line in PROGMEM
 	Each byte in the vertical line is a palette index
@@ -48,18 +55,21 @@
 */
 
 #define LEDS_PER_CHIP	5
-#define CHIPS_COUNT		3
+#define CHIPS_COUNT		6
 #define LINES_PER_CHIP	(LEDS_PER_CHIP*3)
 
-uint8_t anChipOutputLine[CHIPS_COUNT] = {1<<2, 1<<1, 1<<0};
+uint8_t anChipOutputLine[CHIPS_COUNT] = {1<<2, 1<<1, 1<<0, 1<<3, 1<<4, 1<<5};
 
 void doDisplayUpdate(uint8_t idxHorizontalPixel) {
 
 	static uint8_t nColumnData[COLUMN_DATA_BYTES];
 	
 	if ( (idxHorizontalPixel<HORZ_PIXELS) && (idxHorizontalPixel<GRAPHIC_WIDTH)) {
-		// Prepare a column to send
 		
+		// Prepare a column to send
+		/* 	if this array was reoriented we could save 2 clocks / loop
+			because there is an instruction to load PROGMEM data and increment the pointer
+		*/
 		uint8_t h = (VERTICAL_PIXELS<GRAPHIC_HEIGHT) ? VERTICAL_PIXELS : GRAPHIC_HEIGHT;
 		for (int idxRow=0; idxRow<h; idxRow++) {
 #			ifdef PROGMEM_GRAPHIC
@@ -67,15 +77,33 @@ void doDisplayUpdate(uint8_t idxHorizontalPixel) {
 #			else /* PROGMEM_GRAPHIC */
 				uint8_t nPaletteIndex = graphic[idxRow][idxHorizontalPixel];
 #			endif /* PROGMEM_GRAPHIC */
-			
+		
+#define OPTIMIZE_UNROLLCOLORPALETTELOOP			/* No significant improvement from this optimization */
+#ifdef OPTIMIZE_UNROLLCOLORPALETTELOOP
+#				ifdef PRECOMPUTE_PALETTE
+				nColumnData[idxRow*3+0] = palette[nPaletteIndex*3+0];
+				nColumnData[idxRow*3+1] = palette[nPaletteIndex*3+1];
+				nColumnData[idxRow*3+2] = palette[nPaletteIndex*3+2];
+#				else /* PRECOMPUTE_PALETTE */
+				nColumnData[idxRow*3+0] = (1<<palette[nPaletteIndex*3+0]) - 1;
+				nColumnData[idxRow*3+1] = (1<<palette[nPaletteIndex*3+1]) - 1;
+				nColumnData[idxRow*3+2] = (1<<palette[nPaletteIndex*3+2]) - 1;
+#				endif /* PRECOMPUTE_PALETTE */
+#else			
 			// Copy the color stored in the palette into the column data
 			for (int idxColor = 0; idxColor<3; idxColor++) {
 				// The palette color is 3 bytes, so we need to copy 3 bytes
 				// Secondly the palette color is stored as an intensity level, 
 				//    a number in range 0-4, or 0-8, or 0-12
 				// We need to convert that to 2^(intensity) - intensity is perceive exponentialy
+#				ifdef PRECOMPUTE_PALETTE
+				nColumnData[idxRow*3+idxColor] = palette[nPaletteIndex*3+idxColor];
+#				else /* PRECOMPUTE_PALETTE */
 				nColumnData[idxRow*3+idxColor] = (1<<palette[nPaletteIndex*3+idxColor]) - 1;
+#				endif /* PRECOMPUTE_PALETTE */
+				
 			}
+#endif			
 		}
 		
 		/* 	The column data array now is a list of (exponentially corrected) intensities,
@@ -98,28 +126,75 @@ void doDisplayUpdate(uint8_t idxHorizontalPixel) {
 			}
 			
 			// Step 2: Send real data for the low 8 bits
-			
-			for (int idxBit = 7; idxBit>=0; idxBit--) {
-				// Prepare a parallel set of bits to send
-				uint8_t nSinData=0;
-				for (int idxChip = 0 ; idxChip<CHIPS_COUNT; idxChip++) {
-					if (nColumnData[idxChip*LINES_PER_CHIP+idxChan] & (1<<idxBit)) {
-						nSinData |= anChipOutputLine[idxChip];
-					} // else: nSinData is already zero
-				}
-			
+#undef OPTIMIZE_HIGHONCE			/* This turns out to be slower in practice. */
+#ifdef OPTIMIZE_HIGHONCE
+			uint8_t nSinData = 0;
+			uint8_t nBitMask = 0x80; 	// Faster than a bit index loop
+			do {
+				if (!(nSinData & anChipOutputLine[0]) && (nColumnData[0*LINES_PER_CHIP+idxChan] & nBitMask)) nSinData |= anChipOutputLine[0]; // else: nSinData is already zero
+				if (!(nSinData & anChipOutputLine[1]) && (nColumnData[0*LINES_PER_CHIP+idxChan] & nBitMask)) nSinData |= anChipOutputLine[1]; // else: nSinData is already zero
+				if (!(nSinData & anChipOutputLine[2]) && (nColumnData[0*LINES_PER_CHIP+idxChan] & nBitMask)) nSinData |= anChipOutputLine[2]; // else: nSinData is already zero
+#if CHIPS_COUNT>3
+				if (!(nSinData & anChipOutputLine[3]) && (nColumnData[0*LINES_PER_CHIP+idxChan] & nBitMask)) nSinData |= anChipOutputLine[3]; // else: nSinData is already zero
+				if (!(nSinData & anChipOutputLine[4]) && (nColumnData[0*LINES_PER_CHIP+idxChan] & nBitMask)) nSinData |= anChipOutputLine[4]; // else: nSinData is already zero
+				if (!(nSinData & anChipOutputLine[5]) && (nColumnData[0*LINES_PER_CHIP+idxChan] & nBitMask)) nSinData |= anChipOutputLine[5]; // else: nSinData is already zero
+#endif			
 				TLC5940_SCLK_PORT 	&= ~(1<<TLC5940_SCLK_BIT);	// SCLK->low
 				TLC5940_SIN_PORT = nSinData;					// Data
 				TLC5940_SCLK_PORT 	|= 1<<TLC5940_SCLK_BIT;		// SCLK->high 
-			}
+			
+				// Move to the next bitmask
+				nBitMask >>= 1;
+			} while (nBitMask);
+#else						
+			uint8_t nBitMask = 0x80; 	// Faster than a bit index loop
+			do {
+				// Prepare a parallel set of bits to send
+				uint8_t nSinData=0;
+				// Unrolled chips loop
+				if (nColumnData[0*LINES_PER_CHIP+idxChan] & (nBitMask)) nSinData |= anChipOutputLine[0]; // else: nSinData is already zero
+				if (nColumnData[1*LINES_PER_CHIP+idxChan] & (nBitMask)) nSinData |= anChipOutputLine[1]; // else: nSinData is already zero
+				if (nColumnData[2*LINES_PER_CHIP+idxChan] & (nBitMask)) nSinData |= anChipOutputLine[2]; // else: nSinData is already zero
+#if CHIPS_COUNT>3
+				if (nColumnData[3*LINES_PER_CHIP+idxChan] & (nBitMask)) nSinData |= anChipOutputLine[3]; // else: nSinData is already zero
+				if (nColumnData[4*LINES_PER_CHIP+idxChan] & (nBitMask)) nSinData |= anChipOutputLine[4]; // else: nSinData is already zero
+				if (nColumnData[5*LINES_PER_CHIP+idxChan] & (nBitMask)) nSinData |= anChipOutputLine[5]; // else: nSinData is already zero
+#endif			
+				TLC5940_SCLK_PORT 	&= ~(1<<TLC5940_SCLK_BIT);	// SCLK->low
+				TLC5940_SIN_PORT = nSinData;					// Data
+				TLC5940_SCLK_PORT 	|= 1<<TLC5940_SCLK_BIT;		// SCLK->high 
+				
+				// Move to the next bitmask
+				nBitMask >>= 1;
+			} while (nBitMask);
+
+#endif			
 				
 		} // channel loop
 		
 		TLC5940_XLAT_PORT |= 1<<TLC5940_XLAT_BIT;			// XLAT -> high
 		
+	} else {
+		// Send black
+		TLC5940_SIN_PORT 	= 0;
 		
+#define OPTB		
+#ifdef OPTB		
+		uint8_t idxBit = LINES_PER_CHIP*12;
+		do {
+			TLC5940_SCLK_PORT 	&= ~(1<<TLC5940_SCLK_BIT);	// SCLK->low
+			--idxBit;
+			TLC5940_SCLK_PORT 	|= 1<<TLC5940_SCLK_BIT;		// SCLK->high 
+															// Rising edge of the clock signal clocks in the data
+		} while (idxBit);
+#else		
+		for (uint8_t idxBit=0;idxBit<LINES_PER_CHIP*12;idxBit++) {
+			TLC5940_SCLK_PORT 	&= ~(1<<TLC5940_SCLK_BIT);	// SCLK->low
+			TLC5940_SCLK_PORT 	|= 1<<TLC5940_SCLK_BIT;		// SCLK->high 
+															// Rising edge of the clock signal clocks in the data
+		}
+#endif		
 	}
-	
 }
 
 void doDisplayInit() {
@@ -156,5 +231,16 @@ void doDisplayInit() {
 	TLC5940_VPRG_PORT &= ~(1<<TLC5940_VPRG_BIT);
 	// DCPRG -> low
 	TLC5940_DCPRG_PORT &= ~(1<<TLC5940_DCPRG_BIT);
-		
+
+	// Precompute the palette
+#	ifdef PRECOMPUTE_PALETTE
+		for (int idxPal = 0; idxPal<PALETTE_SIZE; idxPal++) {
+			for (int idxColor = 0; idxColor<3; idxColor++) {
+				palette[idxPal*3+idxColor] = (1<<palette[idxPal*3+idxColor]) - 1;
+			}
+		}
+#	else /* PRECOMPUTE_PALETTE */
+		// Nothing to do 
+#	endif /* PRECOMPUTE_PALETTE */
+	
 }
